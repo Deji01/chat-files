55e9b30d7f5fd5d8c39a73e531f75f1106f46799
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-01-02 23:16:18 +0000

    config

---
+++ doc-chat/app/chat/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ChatPage() {
+    return (
+        <div className="max-w-6xl flex flex-col items-center w-full h-full">
+            <div className="flex flex-col w-full gap-6 grow my-2 sm:my-10 p-4 sm:p-8 sm:border rounded-sm overflow-y-auto">
+            <div className="border-slate-400 rounded-lg flex flex-col justify-start gap-4 pr-2 grow overflow-y-scroll">
+                
+            </div>
+            </div>
+        </div>
+    )
+}
--- doc-chat/app/layout.tsx
+++ doc-chat/app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "AI Chat App",
+  description: "The fastest way to chat with your documents",
 };
 
 export default function RootLayout({
